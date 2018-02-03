42c40c0342b3aa3fe8d977334074aaa8ec11893a
Esko Luontola <esko@luontola.fi>
2018-02-03 22:10:45 +0200

    Refactor

--- src/Matopeli.js
+++ src/Matopeli.js
@@ -13,10 +13,10 @@ function createWorld() {
     width: 30,
     height: 20,
   };
-  const allPositions = [];
+  const allCells = [];
   for (let x = 0; x < world.width; x++) {
     for (let y = 0; y < world.width; y++) {
-      allPositions.push({x, y});
+      allCells.push({x, y});
     }
   }
 
@@ -26,24 +26,49 @@ function createWorld() {
   };
   world.worm = [head];
   world.direction = {x: 1, y: 0};
+  world.target = randomEmptyCell();
 
-  world.isEmpty = (position) => {
+  function sumVectors(v1, v2) {
+    return {
+      x: v1.x + v2.x,
+      y: v1.y + v2.y,
+    }
+  }
+
+  function isEmptyCell(cell) {
     for (const segment of world.worm) {
-      if (isEqual(segment, position)) {
+      if (isEqual(segment, cell)) {
         return false;
       }
     }
     return true;
-  };
+  }
 
-  world.addTarget = () => {
-    const emptyPositions = allPositions.filter(world.isEmpty);
-    if (emptyPositions.length > 0) {
-      const randomIndex = random(0, emptyPositions.length);
-      world.target = emptyPositions[randomIndex];
+  function randomEmptyCell() {
+    const emptyCells = allCells.filter(isEmptyCell);
+    if (emptyCells.length <= 0) {
+      return null;
     }
-  };
-  world.addTarget();
+    const randomIndex = random(0, emptyCells.length);
+    return emptyCells[randomIndex];
+  }
+
+  function move(worm, direction) {
+    const head = worm[0];
+    const newHead = sumVectors(head, direction);
+    const newTail = worm.slice(0, -1); // TODO: not tested
+    return [newHead, ...newTail];
+  }
+
+  function hitsWalls(worm, world) {
+    for (const segment of worm) {
+      if (segment.x < 0 || segment.x >= world.width ||
+        segment.y < 0 || segment.y >= world.height) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   world.changeDirection = (direction) => {
     const turn180 = isEqual(sumVectors(world.direction, direction), {x: 0, y: 0});
@@ -52,53 +77,20 @@ function createWorld() {
     }
   };
 
+  world.simulate = () => {
+    if (world.gameOver) {
+      return;
+    }
+    const newWorm = move(world.worm, world.direction);
+    if (hitsWalls(newWorm, world)) {
+      world.gameOver = true;
+    } else {
+      world.worm = newWorm;
+    }
+  };
+
   return world;
 }
-
-
-// Helpers
-
-function sumVectors(v1, v2) {
-  return {
-    x: v1.x + v2.x,
-    y: v1.y + v2.y,
-  }
-}
-
-
-// Simulation
-
-function move(worm, direction) {
-  const head = worm[0];
-  const newHead = sumVectors(head, direction);
-  const newTail = worm.slice(0, -1); // TODO: not tested
-  return [newHead, ...newTail];
-}
-
-function hitsWalls(worm, world) {
-  for (const segment of worm) {
-    if (segment.x < 0 || segment.x >= world.width ||
-      segment.y < 0 || segment.y >= world.height) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function simulateWorld(world) {
-  if (world.gameOver) {
-    return;
-  }
-  const newWorm = move(world.worm, world.direction);
-  if (hitsWalls(newWorm, world)) {
-    world.gameOver = true;
-  } else {
-    world.worm = newWorm;
-  }
-}
-
-
-// Rendering
 
 function renderWorld(world, canvas) {
   const ctx = canvas.getContext('2d');
@@ -153,7 +145,7 @@ function initGame(canvas) {
   setInterval(() => renderWorld(world, canvas), 1000.0 / rendererHz);
 
   const simulationHz = 10;
-  setInterval(() => simulateWorld(world), 1000.0 / simulationHz);
+  setInterval(() => world.simulate(), 1000.0 / simulationHz);
 
   document.addEventListener('keydown', (event) => {
     if (event.code === 'Space') {
